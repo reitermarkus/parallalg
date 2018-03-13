7b595c9c5a652fb47701636e1f1c658fbc10ace9
H.Gndgn <hueseyin.guendogan@gmail.com>
2018-03-13 11:41:54 +0100

    add posix header

--- week_02/matrix_multiplication/common/headers.h
+++ week_02/matrix_multiplication/common/headers.h
@@ -1,9 +1,12 @@
 #ifndef __HEADERS_H__
 #define __HEADERS_H__
 
+#define _POSIX_C_SOURCE 199309L
+
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <time.h>
+#include <unistd.h>
 
 #endif
