8308ae26c4536ba846b24358a2dc74f8ee3b2896
Markus Reiter <me@reitermark.us>
2018-03-16 13:29:23 +0100

    Share `printTemperature` function.

---
+++ week_03/heat_stencil/common/helpers.h
@@ -0,0 +1,71 @@
+#pragma once
+
+typedef float value_t;
+typedef value_t* Matrix;
+
+Matrix createMatrix(int N, int M) {
+    // create data and index vector
+    return malloc(sizeof(value_t)*N*M);
+}
+
+void releaseMatrix(Matrix m) {
+    free(m);
+}
+
+void printTemperature(Matrix m, int N, int M) {
+    const char* colors = " .-:=+*#%@";
+    const int numColors = 10;
+
+    // boundaries for temperature (for simplicity hard-coded)
+    const value_t max = 273 + 30;
+    const value_t min = 273 + 0;
+
+    // set the 'render' resolution
+    int H = 30;
+    int W = 50;
+
+    // step size in each dimension
+    int sH = N/H;
+    int sW = M/W;
+
+
+    // upper wall
+    for(int i=0; i<W+2; i++) {
+        printf("X");
+    }
+    printf("\n");
+
+    // room
+    for(int i=0; i<H; i++) {
+        // left wall
+        printf("X");
+        // actual room
+        for(int j=0; j<W; j++) {
+
+            // get max temperature in this tile
+            value_t max_t = 0;
+            for(int x=sH*i; x<sH*i+sH; x++) {
+                for(int y=sW*j; y<sW*j+sW; y++) {
+                    max_t = (max_t < m[x*N+y]) ? m[x*N+y] : max_t;
+                }
+            }
+            value_t temp = max_t;
+
+            // pick the 'color'
+            int c = ((temp - min) / (max - min)) * numColors;
+            c = (c >= numColors) ? numColors-1 : ((c < 0) ? 0 : c);
+
+            // print the average temperature
+            printf("%c",colors[c]);
+        }
+        // right wall
+        printf("X\n");
+    }
+
+    // lower wall
+    for(int i=0; i<W+2; i++) {
+        printf("X");
+    }
+    printf("\n");
+
+}
--- week_03/heat_stencil/heat_stencil_seq.c
+++ week_03/heat_stencil/heat_stencil_seq.c
@@ -3,21 +3,7 @@
 
 #include <utils.h>
 
-typedef float value_t;
-
-
-// -- matrix utilities --
-
-typedef value_t* Matrix;
-
-Matrix createMatrix(int N, int M);
-
-void releaseMatrix(Matrix m);
-
-void printTemperature(Matrix m, int N, int M);
-
-// ----------------------
-
+#include "common/helpers.h"
 
 int main(int argc, char** argv) {
 
@@ -124,72 +110,3 @@ int main(int argc, char** argv) {
     // done
     return (success) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
-
-
-Matrix createMatrix(int N, int M) {
-    // create data and index vector
-    return malloc(sizeof(value_t)*N*M);
-}
-
-void releaseMatrix(Matrix m) {
-    free(m);
-}
-
-void printTemperature(Matrix m, int N, int M) {
-    const char* colors = " .-:=+*#%@";
-    const int numColors = 10;
-
-    // boundaries for temperature (for simplicity hard-coded)
-    const value_t max = 273 + 30;
-    const value_t min = 273 + 0;
-
-    // set the 'render' resolution
-    int H = 30;
-    int W = 50;
-
-    // step size in each dimension
-    int sH = N/H;
-    int sW = M/W;
-
-
-    // upper wall
-    for(int i=0; i<W+2; i++) {
-        printf("X");
-    }
-    printf("\n");
-
-    // room
-    for(int i=0; i<H; i++) {
-        // left wall
-        printf("X");
-        // actual room
-        for(int j=0; j<W; j++) {
-
-            // get max temperature in this tile
-            value_t max_t = 0;
-            for(int x=sH*i; x<sH*i+sH; x++) {
-                for(int y=sW*j; y<sW*j+sW; y++) {
-                    max_t = (max_t < m[x*N+y]) ? m[x*N+y] : max_t;
-                }
-            }
-            value_t temp = max_t;
-
-            // pick the 'color'
-            int c = ((temp - min) / (max - min)) * numColors;
-            c = (c >= numColors) ? numColors-1 : ((c < 0) ? 0 : c);
-
-            // print the average temperature
-            printf("%c",colors[c]);
-        }
-        // right wall
-        printf("X\n");
-    }
-
-    // lower wall
-    for(int i=0; i<W+2; i++) {
-        printf("X");
-    }
-    printf("\n");
-
-}
-
