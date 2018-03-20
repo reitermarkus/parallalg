af626b9aefc4a7430feead5fd9f78a37b9df0a30
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2018-03-20 21:50:53 +0100

    Fix spacing.

--- week_03/heat_stencil/heat_stencil_ocl.c
+++ week_03/heat_stencil/heat_stencil_ocl.c
@@ -37,7 +37,7 @@ void init_devices() {
   vec_size = sizeof(value_t) * n * n;
   dev_vec_temperature = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_HOST_WRITE_ONLY, vec_size, NULL, &ret);
   dev_vec_compute = clCreateBuffer(context, CL_MEM_READ_WRITE | CL_MEM_HOST_WRITE_ONLY, vec_size, NULL, &ret);
-  
+
   ret = clEnqueueWriteBuffer(command_queue, dev_vec_temperature, CL_TRUE, 0, vec_size, &mtx_temperature[0], 0, NULL, NULL);
   ret = clEnqueueWriteBuffer(command_queue, dev_vec_compute, CL_TRUE, 0, vec_size, &mtx_compute[0], 0, NULL, NULL);
 }
@@ -86,7 +86,7 @@ void clean_up() {
   ret = clFinish(command_queue);
   ret = clReleaseKernel(kernel);
   ret = clReleaseProgram(program);
-  
+
   // free device memory
   ret = clReleaseMemObject(dev_vec_temperature);
   ret = clReleaseMemObject(dev_vec_compute);
@@ -97,99 +97,97 @@ void clean_up() {
 }
 
 void print(Matrix mtx) {
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            printf("%f ", mtx[i * n + j]);
-        }
-        puts("");
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < n; j++) {
+        printf("%f ", mtx[i * n + j]);
     }
+    puts("");
+  }
 }
 
 int main(int argc, char** argv) {
+  const char *program_name = "heat_stencil.cl";
+  const char *kernel_name = "heat_stencil";
 
-    const char *program_name = "heat_stencil.cl";
-    const char *kernel_name = "heat_stencil";
+  // 'parsing' optional input parameter = problem size
+  if (argc > 1) {
+    n = atoi(argv[1]);
+  }
+  int T = n * 100;
+  printf("Computing heat-distribution for room size n=%d for T=%d timesteps\n", n, T);
 
-    // 'parsing' optional input parameter = problem size
-    if (argc > 1) {
-        n = atoi(argv[1]);
+  // create a buffer for storing temperature fields
+  mtx_temperature = create_matrix(n,n);
+
+  // set up initial conditions in mtx_temperature
+  for(int i = 0; i < n; i++) {
+    for(int j = 0; j < n; j++) {
+      mtx_temperature[i * n + j] = 273.0f;             // temperature is 0°C everywhere (273K)
     }
-    int T = n * 100;
-    printf("Computing heat-distribution for room size n=%d for T=%d timesteps\n", n, T);
+  }
 
-    // create a buffer for storing temperature fields
-    mtx_temperature = create_matrix(n,n);
+  // and there is a heat source in one corner
+  int source_x = n / 4;
+  int source_y = n / 4;
+  mtx_temperature[source_x * n + source_y] = 273.0f + 60.0f;
 
-    // set up initial conditions in mtx_temperature
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < n; j++) {
-            mtx_temperature[i * n + j] = 273.0f;             // temperature is 0°C everywhere (273K)
-        }
+  printf("Initial:\n");
+  print_temperature(mtx_temperature, n, n);
+
+  // ---------- compute ----------
+
+  // create a second buffer for the computation
+  mtx_compute = create_matrix(n, n);
+
+  timestamp begin = now();
+
+  init_platform();
+  init_devices();
+  create_program(program_name);
+
+  // for each time step ..
+  for(int t=0; t<T; t++) {
+    run_kernel(kernel_name, source_x, source_y);
+
+    // swap matrixes (just pointers, not content)
+    Matrix tmp = mtx_temperature;
+    mtx_temperature = mtx_compute;
+    mtx_compute = tmp;
+
+    // show intermediate step
+    if (!(t%1000)) {
+      printf("Step t=%d:\n", t);
+      print_temperature(mtx_temperature,n,n);
     }
+  }
 
-    // and there is a heat source in one corner
-    int source_x = n / 4;
-    int source_y = n / 4;
-    mtx_temperature[source_x * n + source_y] = 273.0f + 60.0f;
 
-    printf("Initial:\n");
-    print_temperature(mtx_temperature, n, n);
+  timestamp end = now();
+  printf("Total time: %.3fms\n", (end - begin)*1000);
 
-    // ---------- compute ----------
+  // ---------- check ----------
 
-    // create a second buffer for the computation
-    mtx_compute = create_matrix(n, n);
+  printf("Final:\n");
+  print_temperature(mtx_temperature, n, n);
 
-    timestamp begin = now();
-
-    init_platform();
-    init_devices();
-    create_program(program_name);
-
-    // for each time step ..
-    for(int t=0; t<T; t++) {
-
-        run_kernel(kernel_name, source_x, source_y);
-
-        // swap matrixes (just pointers, not content)
-        Matrix tmp = mtx_temperature;
-        mtx_temperature = mtx_compute;
-        mtx_compute = tmp;
-
-        // show intermediate step
-        if (!(t%1000)) {
-            printf("Step t=%d:\n", t);
-            print_temperature(mtx_temperature,n,n);
-        }
+  bool success = true;
+  for(long long i = 0; i<n; i++) {
+    for(long long j = 0; j<n; j++) {
+      value_t temp = mtx_temperature[i*n+j];
+      if (273.0f <= temp && temp <= 273.0f + 60.0f) continue;
+      success = false;
+      break;
     }
+  }
 
+  printf("Verification: %s\n", (success) ? "OK" : "FAILED");
 
-    timestamp end = now();
-    printf("Total time: %.3fms\n", (end - begin)*1000);
+  // ---------- cleanup ----------
 
-    // ---------- check ----------
+  clean_up();
+  release_matrix(mtx_compute);
+  release_matrix(mtx_temperature);
 
-    printf("Final:\n");
-    print_temperature(mtx_temperature, n, n);
-
-    bool success = true;
-    for(long long i = 0; i<n; i++) {
-        for(long long j = 0; j<n; j++) {
-            value_t temp = mtx_temperature[i*n+j];
-            if (273.0f <= temp && temp <= 273.0f + 60.0f) continue;
-            success = false;
-            break;
-        }
-    }
-
-    printf("Verification: %s\n", (success) ? "OK" : "FAILED");
-
-    // ---------- cleanup ----------
-
-    clean_up();
-    release_matrix(mtx_compute);
-    release_matrix(mtx_temperature);
-
-    // done
-    return (success) ? EXIT_SUCCESS : EXIT_FAILURE;
+  // done
+  return (success) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
