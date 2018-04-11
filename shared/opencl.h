f3093fe231b3910f096da21b973c59c8eee1db6d
Markus Reiter <me@reitermark.us>
2018-04-12 01:03:24 +0200

    Fix `clCreateCommandQueueWithProperties` on macOS.

--- shared/opencl.h
+++ shared/opencl.h
@@ -1,16 +1,15 @@
 #ifndef __OPEN_CL_CROSS_PLATFORM_H__
 #define __OPEN_CL_CROSS_PLATFORM_H__
 
+#define CL_USE_DEPRECATED_OPENCL_1_0_APIS
+#define CL_USE_DEPRECATED_OPENCL_1_1_APIS
 #define CL_USE_DEPRECATED_OPENCL_1_2_APIS
 #if __APPLE__
-  #include <OpenCL/cl.h>
+  #include <OpenCL/opencl.h>
 
-  #define clCreateCommandQueueWithProperties clCreateCommandQueue
-
-  // #undef CL_MEM_HOST_WRITE_ONLY
-  // #define CL_MEM_HOST_WRITE_ONLY 0
-  // #undef CL_MEM_HOST_READ_ONLY
-  // #define CL_MEM_HOST_READ_ONLY 0
+  #undef cl_command_queue_properties
+  #define cl_command_queue_properties cl_queue_properties_APPLE
+  #define clCreateCommandQueueWithProperties clCreateCommandQueueWithPropertiesAPPLE
 #else
   #include <CL/cl.h>
 #endif
