10eb71e4ea488524e6c70c1bfa4bc53d6d824a44
Markus Reiter <me@reitermark.us>
2018-04-23 21:12:33 +0200

    Use local memory in Rust version.

--- week_05/heat_stencil-rs/src/main_ocl.rs
+++ week_05/heat_stencil-rs/src/main_ocl.rs
@@ -9,28 +9,64 @@ use print_temperature::print_temperature;
 #[macro_use]
 mod benchmark;
 
+macro_rules! multiple {
+  ($v:expr, $d:expr) => {{
+    let rest = $v % $d;
+    if rest == 0 { $v } else { $v + ($d - rest) }
+  }}
+}
+
 fn temp() -> ocl::Result<()> {
   let kernel_source = r#"
-    __kernel void calc_temp(__global float const* matrix_a, __global float* matrix_b, ulong n, ulong source_x, ulong source_y) {
+    kernel void calc_temp(global float const* matrix_a, global float* matrix_b, local float* t, ulong n, ulong source_x, ulong source_y) {
       size_t i = get_global_id(0);
       size_t j = get_global_id(1);
 
+      size_t w = get_local_size(0);
+      size_t h = get_local_size(1);
+
+      size_t li = get_local_id(0);
+      size_t lj = get_local_id(1);
+
+      size_t bs = w + 2;
+
+      #define G(X,Y) matrix_a[   (X) * n +   (Y)  ]
+      #define L(X,Y) t[((X)+1)*bs + ((Y)+1)]
+
+      if (i < n && j < n) {
+        // Get center.
+        L(li,lj) = G(i,j);
+
+        // Get current temperatures left/right.
+        if (li ==    0  ) L(li - 1, lj) = G(i - 1, j);
+        if (li == w - 1) L(li + 1, lj) = G(i + 1, j);
+
+        // Get current temperatures up/down.
+        if (lj ==    0  ) L(li, lj - 1) = G(i, j - 1);
+        if (lj == h - 1) L(li, lj + 1) = G(i, j + 1);
+      }
+
+      barrier(CLK_LOCAL_MEM_FENCE);
+
+      if (i >= n || j >= n) return;
+
       // Center stays constant (the heat is still on).
       if (i == source_x && j == source_y) {
-        matrix_b[i * n + j] = matrix_a[i * n + j];
-      } else {
-        // Get current temperature at (i,j).
-        float tc = matrix_a[i * n + j];
-
-        // Get temperatures left/right and up/down.
-        float tl = j !=  0     ? matrix_a[i * n + (j - 1)] : tc;
-        float tr = j != n - 1  ? matrix_a[i * n + (j + 1)] : tc;
-        float tu = i !=  0     ? matrix_a[(i - 1) * n + j] : tc;
-        float td = i != n - 1  ? matrix_a[(i + 1) * n + j] : tc;
-
-        // Update temperature at current point.
-        matrix_b[i * n + j] = tc + 0.2 * (tl + tr + tu + td + (-4.0 * tc));
+        matrix_b[i * n + j] = L(li, lj);
+        return;
       }
+
+      // Get current temperature at (i, j).
+      float tc = L(li, lj);
+
+      // Get temperatures left/right and up/down.
+      float tl = (j !=   0  ) ? L(li, lj - 1) : tc;
+      float tr = (j != n - 1) ? L(li, lj + 1) : tc;
+      float tu = (i !=   0  ) ? L(li - 1, lj) : tc;
+      float td = (i != n - 1) ? L(li + 1, lj) : tc;
+
+      // Update temperature at current point.
+      matrix_b[i * n + j] = tc + 0.2f * (tl + tr + tu + td + (-4.0f * tc));
     }
   "#;
 
@@ -58,11 +94,15 @@ fn temp() -> ocl::Result<()> {
 
   let time_steps = n * 100;
 
+  let local_work_size = [16, 16];
+
   let kernel = pro_que.kernel_builder("calc_temp")
                       .global_work_offset([0, 0])
-                      .global_work_size([n, n])
+                      .local_work_size(local_work_size)
+                      .global_work_size([multiple!(n, local_work_size[0]), multiple!(n, local_work_size[1])])
                       .arg_named("input", None::<&Buffer<f32>>)
                       .arg_named("output", None::<&Buffer<f32>>)
+                      .arg_local::<f32>((local_work_size[0] + 2) * (local_work_size[1] + 2))
                       .arg(&n)
                       .arg(&source_x)
                       .arg(&source_y)
