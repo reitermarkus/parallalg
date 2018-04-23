f80a1331cf649ed69e00b2840f08393d01d5870e
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2018-04-24 00:36:06 +0200

    Use `heat_stencil.cl` instead of raw string in Rust version.

--- week_05/heat_stencil-rs/src/main_ocl.rs
+++ week_05/heat_stencil-rs/src/main_ocl.rs
@@ -4,6 +4,8 @@ use ocl::enums::DeviceInfo;
 use ocl::enums::DeviceInfoResult::MaxWorkGroupSize;
 
 use std::mem;
+use std::fs::File;
+use std::io::{Read, BufReader};
 
 mod print_temperature;
 use print_temperature::print_temperature;
@@ -19,58 +21,10 @@ macro_rules! multiple {
 }
 
 fn temp() -> ocl::Result<()> {
-  let kernel_source = r#"
-    kernel void calc_temp(global float const* matrix_a, global float* matrix_b, local float* t, ulong n, ulong source_x, ulong source_y) {
-      size_t i = get_global_id(0);
-      size_t j = get_global_id(1);
-
-      size_t w = get_local_size(0);
-      size_t h = get_local_size(1);
-
-      size_t li = get_local_id(0);
-      size_t lj = get_local_id(1);
-
-      size_t bs = w + 2;
-
-      #define G(X,Y) matrix_a[   (X) * n +   (Y)  ]
-      #define L(X,Y) t[((X)+1)*bs + ((Y)+1)]
-
-      if (i < n && j < n) {
-        // Get center.
-        L(li,lj) = G(i,j);
-
-        // Get current temperatures left/right.
-        if (li ==    0  ) L(li - 1, lj) = G(i - 1, j);
-        if (li == w - 1) L(li + 1, lj) = G(i + 1, j);
-
-        // Get current temperatures up/down.
-        if (lj ==    0  ) L(li, lj - 1) = G(i, j - 1);
-        if (lj == h - 1) L(li, lj + 1) = G(i, j + 1);
-      }
-
-      barrier(CLK_LOCAL_MEM_FENCE);
-
-      if (i >= n || j >= n) return;
-
-      // Center stays constant (the heat is still on).
-      if (i == source_x && j == source_y) {
-        matrix_b[i * n + j] = L(li, lj);
-        return;
-      }
-
-      // Get current temperature at (i, j).
-      float tc = L(li, lj);
-
-      // Get temperatures left/right and up/down.
-      float tl = (j !=   0  ) ? L(li, lj - 1) : tc;
-      float tr = (j != n - 1) ? L(li, lj + 1) : tc;
-      float tu = (i !=   0  ) ? L(li - 1, lj) : tc;
-      float td = (i != n - 1) ? L(li + 1, lj) : tc;
-
-      // Update temperature at current point.
-      matrix_b[i * n + j] = tc + 0.2f * (tl + tr + tu + td + (-4.0f * tc));
-    }
-  "#;
+  let file = File::open("../heat_stencil/heat_stencil.cl")?;
+  let mut buf_reader = BufReader::new(file);
+  let mut kernel_source = String::new();
+  buf_reader.read_to_string(&mut kernel_source)?;
 
   let n = 500;
 
