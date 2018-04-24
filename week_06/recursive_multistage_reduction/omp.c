2af29dfffb8e530e143bd6a9a104d8b9c493fa3b
H.Gndgn <hueseyin.guendogan@gmail.com>
2018-04-24 13:41:04 +0200

    add part 2

---
+++ week_06/recursive_multistage_reduction/omp.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(int argc, char **argv) {
+
+    srand(time(NULL));
+
+    long n;
+    long count = 0;
+
+    printf("N = ");
+    scanf("%ld", &n);
+
+    long *array = (long *) malloc(sizeof(int) * n);
+
+    for (long i = 0; i < n; i++) {
+        array[i] = (rand() % 2);
+    }
+
+    #pragma omp parallel for
+    for (long i = 0; i < n; i++) {
+        if (array[i] == 1)
+            count++;
+    }
+
+    printf("Count: %ld\n", count);
+    
+    return EXIT_SUCCESS;
+}
