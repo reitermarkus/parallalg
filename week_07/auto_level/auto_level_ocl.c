558b025ba3c8a6c394aaaf8175d63a87e105378b
Markus Reiter <me@reitermark.us>
2018-05-02 22:23:00 +0200

    Add helper function for calling reduce kernels.

--- week_07/auto_level/auto_level_ocl.c
+++ week_07/auto_level/auto_level_ocl.c
@@ -10,6 +10,44 @@
 #include "open_cl.h"
 #include "cl_utils.h"
 
+void reduce(cl_command_queue command_queue, cl_program program, const char* kernel_name, int width, int height, int components, cl_mem input_image, cl_mem result, unsigned long* output) {
+
+  cl_int ret;
+  cl_kernel kernel = clCreateKernel(program, kernel_name, &ret);
+  CLU_ERRCHECK(ret, "Failed to create kernel from program.");
+
+  unsigned long length = width * height;
+  size_t i = 0;
+
+  while (length > 1) {
+    size_t global_work_offset = 0;
+    size_t local_work_size = 256;
+    size_t global_work_size = extend_to_multiple(length, local_work_size);
+
+    cluSetKernelArguments(kernel, 5,
+      sizeof(cl_mem), (void*)(i == 0 ? &input_image : &result),
+      sizeof(cl_ulong) * local_work_size * components, NULL,
+      sizeof(cl_ulong), &length,
+      sizeof(cl_int), &components,
+      sizeof(cl_mem), (void*)&result
+    );
+
+    CLU_ERRCHECK(clEnqueueNDRangeKernel(command_queue, kernel, 1,
+      &global_work_offset, &global_work_size, &local_work_size, 0, NULL, NULL), "Failed to enqueue 1D kernel.");
+
+    length = global_work_size / local_work_size;
+    i++;
+  }
+
+  CLU_ERRCHECK(clEnqueueReadBuffer(command_queue, result,
+    CL_TRUE, 0, sizeof(unsigned long) * components, output, 0, NULL, NULL), "Failed reading back result.");
+
+  CLU_ERRCHECK(clFlush(command_queue), "Failed to flush command queue.");
+  CLU_ERRCHECK(clFinish(command_queue), "Failed to wait for command queue completion.");
+
+  CLU_ERRCHECK(clReleaseKernel(kernel), "Failed to release kernel.");
+}
+
 int main(int argc, char **argv) {
   if (argc != 3) {
     printf("Usage: auto_levels [inputfile] [outputfile]\nExample: %s test.png test_out.png\n", argv[0]);
@@ -54,49 +92,27 @@ int main(int argc, char **argv) {
   cl_kernel kernel = clCreateKernel(program, "reduce_sum", &ret);
   CLU_ERRCHECK(ret, "Failed to create kernel from program.");
 
-  unsigned long length = width * height;
-  size_t i = 0;
+  unsigned long minimum[components];
+  unsigned long maximum[components];
+  unsigned long count[components];
 
-  while (length > 1) {
-    size_t global_work_offset = 0;
-    size_t local_work_size = 256;
-    size_t global_work_size = extend_to_multiple(length, local_work_size);
+  reduce(command_queue, program, "reduce_min", width, height, components, input_image, result, minimum);
+  reduce(command_queue, program, "reduce_max", width, height, components, input_image, result, maximum);
+  reduce(command_queue, program, "reduce_sum", width, height, components, input_image, result, count);
 
-    cluSetKernelArguments(kernel, 5,
-      sizeof(cl_mem), (void*)(i == 0 ? &input_image : &result),
-      sizeof(cl_ulong) * local_work_size * components, NULL,
-      sizeof(cl_ulong), &length,
-      sizeof(cl_int), &components,
-      sizeof(cl_mem), (void*)&result
-    );
-
-    CLU_ERRCHECK(clEnqueueNDRangeKernel(command_queue, kernel, 1,
-      &global_work_offset, &global_work_size, &local_work_size, 0, NULL, NULL), "Failed to enqueue 1D kernel.");
-
-    length = global_work_size / local_work_size;
-    i++;
-  }
-
-  unsigned long long count[components];
-
-  CLU_ERRCHECK(clEnqueueReadBuffer(command_queue, result,
-    CL_TRUE, 0, sizeof(unsigned long long) * components, count, 0, NULL, NULL), "Failed reading back result.");
-
-  // wait for completed operations (there should be none)
-  CLU_ERRCHECK(clFlush(command_queue), "Failed to flush command queue.");
-  CLU_ERRCHECK(clFinish(command_queue), "Failed to wait for command queue completion.");
-  CLU_ERRCHECK(clReleaseKernel(kernel), "Failed to release kernel.");
   CLU_ERRCHECK(clReleaseProgram(program), "Failed to release program.");
 
-  // free device memory
+  // Free device memory.
   CLU_ERRCHECK(clReleaseMemObject(input_image), "Failed to release input buffer.");
   CLU_ERRCHECK(clReleaseMemObject(result), "Failed to release result buffer.");
 
-  // free management resources
+  // Free management resources.
   CLU_ERRCHECK(clReleaseCommandQueue(command_queue), "Failed to release command queue.");
   CLU_ERRCHECK(clReleaseContext(context), "Failed to release OpenCL context.");
 
   for (size_t c = 0; c < components; c++) {
+    printf("Component %1u Min: %u\n", c, minimum[c]);
+    printf("Component %1u Max: %u\n", c, maximum[c]);
     printf("Component %1u Sum: %u\n", c, count[c]);
   }
 
@@ -130,6 +146,8 @@ int main(int argc, char **argv) {
   }
 
   for (int c = 0; c < components; c++) {
+    printf("Component %1u Min: %u\n", c, min_val[c]);
+    printf("Component %1u Max: %u\n", c, max_val[c]);
     printf("Component %1u Sum: %u\n", c, sum[c]);
   }
 
@@ -137,7 +155,7 @@ int main(int argc, char **argv) {
   float min_fac[components];
   float max_fac[components];
   for (int c = 0; c < components; c++) {
-    avg_val[c] = sum[c] / (unsigned long long)(width * height);
+    avg_val[c] = sum[c] / (unsigned long)(width * height);
     min_fac[c] = (float)avg_val[c] / (float)(avg_val[c] - min_val[c]);
     max_fac[c] = (255.0f - (float)avg_val[c]) / (float)(max_val[c] - avg_val[c]);
     printf("\tComponent %1u: %3u / %3u / %3u * %3.2f / %3.2f\n", c, min_val[c], avg_val[c], max_val[c], min_fac[c], max_fac[c]);
