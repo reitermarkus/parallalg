c152797831a321267684ecf93088cd2bae20754f
Michael Kaltschmid <kaltschmidmichael@gmail.com>
2018-05-05 01:41:47 +0200

    Generate map of random ages and names.

--- week_08/count_sort-rs/src/main_seq.rs
+++ week_08/count_sort-rs/src/main_seq.rs
@@ -1,13 +1,36 @@
 extern crate libc;
 use libc::c_char;
-use std::ffi::CStr;
+
+extern crate rand;
+use rand::distributions::{IndependentSample, Range};
+
+use std::{env, ffi::CStr, collections::HashMap};
 
 extern {
   fn gen_name() -> *const c_char;
 }
 
-fn main() {
+fn generate_name() -> String {
   let c_buf = unsafe { gen_name() };
   let c_str = unsafe { CStr::from_ptr(c_buf) };
-  let str_slice = c_str.to_str().unwrap();
+  c_str.to_str().unwrap().to_owned()
+}
+
+fn print_list(list: &HashMap<usize, String>) {
+  for (n, name) in list {
+      println!("{} | {}", n, name);
+  }
+}
+
+fn main() {
+  let args: Vec<String> = env::args().collect();
+  let name_count: usize = args.get(1).map_or(None, |x| x.parse().ok()).unwrap_or(1337);
+
+  let names : HashMap<usize, String> = (0..name_count).map(|_| {
+    let between = Range::new(1, 101);
+    let rng = &mut rand::thread_rng();
+    (between.ind_sample(rng), generate_name())
+  }).collect();
+
+  print_list(&names);
 }
