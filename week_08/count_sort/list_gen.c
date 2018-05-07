9cc93ef099e2d3ec0b26afdad43e7e9a743a257f
H.Gndgn <hueseyin.guendogan@gmail.com>
2018-05-07 23:47:03 +0200

    add countsort for person list

--- week_08/count_sort/list_gen.c
+++ week_08/count_sort/list_gen.c
@@ -1,21 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include "people.h"
+#include "tokenize.h"
 
-void print(int *array, int size) {
-  int i;
-  for (i = 0; i < size - 1; i++) {
-    printf("%d, ", array[i]);
+void print_list(person_t *list, int size) {
+  for (int i = 0; i < size; i++) {
+    printf("[%d]\tName: %s\tAge: %d\n", i + 1, list[i].name, list[i].age);
   }
-  printf("%d\n", array[i]);
 }
 
-int* count_sort(int *input, int size) {
+void count_sort(person_t *input, int size) {
   int max = 0;
 
   // find highest number
   for (int i = 0; i < size; i++) {
-    if (input[i] > max)
-      max = input[i];
+    if (input[i].age > max)
+      max = input[i].age;
   }
 
   // initialize count array of size max with 0's
@@ -23,40 +24,62 @@ int* count_sort(int *input, int size) {
 
   // count occurences
   for (int i = 0; i < size; i++) {
-    count_arr[input[i]]++;
+    count_arr[input[i].age]++;
   }
 
   for (int i = 0; i <= max; i++) {
     int count = 0;
     for (int j = 0; j < size; j++) {
-      if (input[j] < i)
+      if (input[j].age < i)
         count++;
     }
     count_arr[i] = count;
   }
 
-  int *result = (int *)calloc(size, sizeof(int));
+  person_t *result = (person_t *)calloc(size, sizeof(person_t));
 
   for (int i = 0; i < size; i++) {
-    int e = input[i];
-    result[count_arr[e]++] = e;
+    person_t p = input[i];
+    result[count_arr[p.age]++] = p;
   }
 
-  return result;
+  memcpy(input, result, sizeof(person_t) * size);
+  free(result);
+  free(count_arr);
+}
+
+void create_person_list(person_t *persons, int n) {
+  person_t p;
+
+  for (int i = 0; i < n; i++) {
+    p.age = rand() % 120;
+    strcpy(p.name, gen_name());
+    persons[i] = p;
+  }
 }
 
 int main(int argc, char **argv) {
-  const int size = 8;
-  int arr[] = {2, 5, 3, 0, 2, 3, 0, 3};
-  int *input = arr;
+  int size = 10;
+  int seed = 1;
+
+  if (argc > 2) {
+    size = atoi(argv[1]);
+    seed = atoi(argv[2]);
+  }
+
+  srand(seed);
+  printf("Generating list of size %d with seed %d\n\n", size, seed);
+
+  person_t *list = (person_t *)malloc(size * sizeof(person_t));
+  create_person_list(list, size);
 
   puts("Unsorted:");
-  print(arr, size);
+  print_list(list, size);
   
-  input = count_sort(arr, size);
+  count_sort(list, size);
 
-  puts("Sorted:");
-  print(input, size);
+  puts("\nSorted:");
+  print_list(list, size);
 
-  free(input);
+  free(list);
 }
