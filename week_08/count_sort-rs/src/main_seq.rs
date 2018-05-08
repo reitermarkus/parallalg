bd2e454d99344d303c6359df8b75f96ff494f393
H.Gndgn <hueseyin.guendogan@gmail.com>
2018-05-08 12:03:56 +0200

    my first ghetto rust commit (countsort)

--- week_08/count_sort-rs/src/main_seq.rs
+++ week_08/count_sort-rs/src/main_seq.rs
@@ -17,6 +17,44 @@ fn print_list(list: &Vec<Person>) {
   }
 }
 
+fn count_sort(input: Vec<usize>) -> Vec<usize> {
+  let size = input.len();
+  let mut max = 0;
+
+  // find max
+  for i in 0..size {
+    if input[i] > max {
+      max = input[i];
+    }
+  }
+
+  let mut count_arr = vec![0; max + 1];
+
+  for i in 0..size {
+    count_arr[input[i]] += 1;
+  }
+
+  for i in 0..(max + 1) {
+    let mut count = 0;
+    for j in 0..size {
+      if input[j] < i {
+        count += 1;
+      }
+    }
+    count_arr[i] = count;
+  }
+
+  let mut result = vec![0; size];
+
+  for i in 0..size {
+    let mut e = input[i];
+    result[count_arr[e]] = e;
+    count_arr[e] += 1;
+  }
+
+  result
+}
+
 fn main() {
   let args: Vec<String> = env::args().collect();
   let name_count: usize = args.get(1).map_or(None, |x| x.parse().ok()).unwrap_or(1337);
