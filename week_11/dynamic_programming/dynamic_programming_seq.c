3f8ee4ce2010c219a86b1f542aad413f8694949a
Markus Reiter <me@reitermark.us>
2018-06-14 22:56:01 +0200

    Rename variables.

--- week_11/dynamic_programming/dynamic_programming_seq.c
+++ week_11/dynamic_programming/dynamic_programming_seq.c
@@ -8,61 +8,61 @@
 #include "utils.h"
 
 int main(int argc, char** argv) {
-  int minSize = 10;
-  int maxSize = 20;
+  int min_size = 10;
+  int max_size = 20;
 
   // read problem size
-  int N = 2000;
+  int n = 2000;
   if (argc > 1) {
-    N = atoi(argv[1]);
+    n = atoi(argv[1]);
   }
 
-  int S = N + 1;
-  printf("Computing minimum cost for multiplying %d matrices ...\n", N);
+  int s = n + 1;
+  printf("Computing minimum cost for multiplying %d matrices ...\n", n);
 
   // generate random matrix sizes
   srand(0);
-  int* l = malloc(sizeof(int) * S);
-  for (int i = 0; i < S; i++) {
-    l[i] = ((rand() / (float)RAND_MAX) * (maxSize - minSize)) + minSize;
+  int* l = malloc(sizeof(int) * s);
+  for (int i = 0; i < s; i++) {
+    l[i] = ((rand() / (float)RAND_MAX) * (max_size - min_size)) + min_size;
   }
 
   // compute minimum costs
-  int* C = malloc(sizeof(int) * N * N);
+  int* minimum_costs = malloc(sizeof(*minimum_costs) * n * n);
 
   double start = now();
 
   // initialize solutions for costs of single matrix
-  for (int i = 0; i < N; i++) {
-    C[i * N + i] = 0; // there is no multiplication cost for those sub-terms
+  for (int i = 0; i < n; i++) {
+    minimum_costs[i * n + i] = 0; // there is no multiplication cost for those sub-terms
   }
 
   // compute minimal costs for multiplying A_i x ... x A_j
-  for (int d = 1; d < N; d++) {   // < distance between i and j
-    for (int i = 0; i < N; i++) { // < starting at each i
+  for (int d = 1; d < n; d++) {   // < distance between i and j
+    for (int i = 0; i < n; i++) { // < starting at each i
       int j = i + d;              // < compute end j
 
       // stop when exceeding boundary
-      if (j >= N) break;
+      if (j >= n) break;
 
       // find cheapest cut between i and j
       int min = INT_MAX;
       for (int k = i; k < j; k++) {
-        int costs = C[i * N + k] + C[(k + 1) * N + j] + l[i] * l[k + 1] * l[j + 1];
+        int costs = minimum_costs[i * n + k] + minimum_costs[(k + 1) * n + j] + l[i] * l[k + 1] * l[j + 1];
         min = (costs < min) ? costs : min;
       }
 
-      C[i * N + j] = min;
+      minimum_costs[i * n + j] = min;
     }
   }
 
   double end = now();
 
-  printf("Minimal costs: %d FLOPS\n", C[0 * N + N - 1]);
+  printf("Minimal costs: %d FLOPS\n", minimum_costs[0 * n + n - 1]);
   printf("Total time: %.3fs\n", (end - start));
 
   // clean
-  free(C);
+  free(minimum_costs);
   free(l);
 
   // done
